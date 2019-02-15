19797e081cceb853321e48ffd3cc4e021871b0c1
Peyton Runyan <peytonrunyan@Peytons-MacBook-Pro.local>
2019-02-15 13:59:14 -0600

    so I made this bit way harder than it had to be

--- acme.py
+++ acme.py
@@ -30,3 +30,28 @@ class Product:
             return '...boom!'
         else:
             return '...fizzle.'
+
+class BoxingGlove(Product):
+    
+    def __init__(self, name, price=10, weight=10, flammability=.5, identifier=None):
+        if identifier is None:
+            identifier = random.randint(1000000, 9999999)
+        self.name = str(name)
+        self.price = int(price)
+        self.weight = int(weight)
+        self.flammability = float(flammability)
+        self.identifier = int(identifier)
+    
+    def explode(self):
+        return "...it's a glove."
+    
+    def punch(self):
+        x = self.weight
+        if x >= 15:
+            return 'OUCH!'
+        elif x>= 5:
+            return "Hey that hurt!"
+        else:
+            return "That tickles."
+    
+    
